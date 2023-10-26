1e17b1acd02fea3ce8be884dc6082ad1033b6968
Dat Le <datle.hcmut@gmail.com>
2023-10-26 20:06:37 +0700

    - implement find_week_start_end()

--- app/function.py
+++ app/function.py
@@ -1,4 +1,5 @@
 from app import session
+import datetime
 
 def get_station_name():
     try:
@@ -24,3 +25,12 @@ def get_station_id(station_name):
     except Exception as e:
         print(f"Error: {str(e)}")
         return str(e)
+
+def find_week_start_end(week_number, year):
+    first_day = datetime.date(year, 1, 1)
+    day_of_week = first_day.weekday()
+    days_to_subtract = day_of_week - 0
+    start_of_week = first_day - datetime.timedelta(days=days_to_subtract)
+    start_of_desired_week = start_of_week + datetime.timedelta(weeks=week_number)
+    end_of_desired_week = start_of_desired_week + datetime.timedelta(days=6)
+    return start_of_desired_week, end_of_desired_week
